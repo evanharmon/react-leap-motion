a36cae540374559a351e6eaa8ee319b79a8069f6
Evan Harmon <evan.harmon@levvel.io>
2016-09-12 08:20:58 -0400

    Add redux

--- src/index.jsx
+++ src/index.jsx
@@ -20,29 +20,68 @@ const testLeapConnected = () => {
     leap.disconnect();
   });
 };
-// testLeapConnected();
+testLeapConnected();
 
-const leap = new Leap.Controller({
+const leapCtrlr = new Leap.Controller({
   host: '127.0.0.1',
   port: 6437,
   frameEventName: 'animationFrame',
   useAllPlugins: true
 });
-// this app really needs redux in it now - bc it needs to listen for change in
-// connect status
+
+// REDUCERS
+const handsReducer = (state = iList(), action) => {
+  switch (action.type) {
+    case 'ADD':
+      return state.push(action.payload);
+    default:
+      return state;
+  }
+};
+
+const testAddHand = () => {
+  const stateBefore = iList().push(
+    iMap({ id: 0 })
+  );
+  const action = {
+    type: 'ADD',
+    payload: iMap({ id: 1 })
+  };
+  const stateAfter = iList().push(
+    iMap({ id: 0 }),
+    iMap({ id: 1 })
+  );
+  const newState = handsReducer(stateBefore, action);
+  expect(is(stateAfter, newState)).toEqual(true);
+};
+testAddHand();
+
+const reducers = combineReducers({
+  hands: handsReducer
+});
+const store = createStore(reducers);
+
+// REACT COMPONENTS
+const nextHandId = 0;
 class HandsApp extends Component {
   constructor(props) {
     super(props);
 
+    this.onStart = this.onStart.bind(this);
+    this.onStop = this.onStop.bind(this);
+    this.onDeviceStreaming = this.onDeviceStreaming.bind(this);
+    this.onFrame = this.onFrame.bind(this);
+
     this.state = {
       leap: {
-        connected: false
-      },
-      handId: 0
+        connected: false,
+        streaming: false,
+        handsLength: 0
+      }
     };
   }
   componentWillUnmount() {
-    leap.disconnect();
+    leapCtrlr.disconnect();
   }
   render() {
     return (
@@ -50,26 +89,61 @@ class HandsApp extends Component {
         <h1>Leap Motion</h1>
 
         <button
-          onClick={() => {
-            leap.on('connect', () => {
-              this.setState({ leap: { connected: true } });
-              console.log('connected', leap.connected());
-            });
-            leap.connect();
-          }}
+          onClick={() =>
+            this.onStart()
+          }
         >Start App</button>
         <button
-          onClick={() => {
-            leap.on('disconnect', () => {
-              this.setState({ leap: { connected: false } });
-              console.log('disconnected', leap.connected());
-            });
-            leap.disconnect();
-          }}
+          onClick={() =>
+            this.onStop()
+          }
         >Stop App</button>
-        { this.state.leap.connected ? <h2>Connected</h2> : <h2>Disconnected</h2> }
+        { this.state.leap.connected
+          ? <h2>Connected</h2>
+          : <h2>Disconnected</h2> }
+        { this.state.leap.streaming
+          ? <h2>Streaming</h2>
+          : <h2></h2> }
+        { this.state.leap.handsLength
+          ? <h2>{this.state.leap.handsLength}</h2>
+          : <h2></h2> }
       </div>
     );
+  }
+  onStart() {
+    leapCtrlr.on('connect', () => {
+      this.setState({ leap: { connected: true } });
+      this.onDeviceStreaming();
+      this.onFrame();
+    });
+    leapCtrlr.connect();
+  }
+  onStop() {
+    leapCtrlr.on('disconnect', () => {
+      this.setState({ leap: { connected: false } });
+    });
+    leapCtrlr.disconnect();
+  }
+  onDeviceStreaming() {
+    leapCtrlr.on('deviceStreaming', () => {
+      this.setState({
+        leap: {
+          connected: this.state.leap.connected,
+          streaming: true
+        }
+      });
+    });
+  }
+  onFrame() {
+    leapCtrlr.on('frame', frame => {
+      this.setState({
+        leap: {
+          connected: this.state.leap.connected,
+          streaming: this.state.leap.streaming,
+          handsLength: frame.hands.length
+        }
+      });
+    });
   }
 }
 
@@ -81,4 +155,5 @@ const render = () => {
 };
 
 render();
+store.subscribe(render);
 console.log('all tests passed');
